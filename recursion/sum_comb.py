3bd6fc143571aef619f06ed5e99dd57c371a1a38
vinay4669 <115799707+vinay4669@users.noreply.github.com>
2022-12-20 23:02:46 +0530

    Second Commit

--- recursion/sum_comb.py
+++ recursion/sum_comb.py
@@ -1,12 +1,16 @@
 def sum_comb(s, n, c):
+    global a
     for i in range(1, n):
         if s<n:
             sum_comb(s+i, n, f'{c}+{i}')
         elif s>n:
-            sum_comb(s-i, n, c[:-1])
+            return 0
         else:
-            print(c)
+            c = c.strip('+')
+            a.append(c) if c not in a else 1
 
-
-n = 5
+a = []
+n = int(input())
 sum_comb(0, n, '')
+for i in a:
+    print(i)
